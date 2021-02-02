519c3832d05e8e977dba9e3cf9fc33fa5abcb882
Rogério Terciotte <rterciotte@gmail.com>
2021-02-02 12:56:26 -0300

    Gerar cupons para promoção

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -43,6 +43,15 @@ class PromotionsController < ApplicationController
       @promotion.destroy
       redirect_to promotions_path
     end
+
+    def generate_coupons
+      @promotion = Promotion.find(params[:id])
+      (1..@promotion.coupon_quantity).each do |number|
+        Coupon.create(code: "#{@promotion.code}-#{'%04d' % number}", promotion: @promotion)
+      end
+      flash[:notice] = 'Cupons gerados com sucesso'
+      redirect_to @promotion
+    end
   
     private
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :promotions do
     get 'search', on: :collection
+    post 'generate_coupons', on: :member
   end
 end
